80b0a1b735928caa9e2e9e5b65b8a065d8ab5e91
tony.vu <tony.vu@temando.com>
2016-11-17 16:33:46 +1100

    magento 2

--- install.py
+++ install.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+
+class bcolor:
+    TEXT = '\033[95m'
+    ENDC = '\033[0m'
+
 import os
 import sys
 # require to install MySQLdb module
@@ -6,13 +11,21 @@ import sys
 import MySQLdb 
 import time
 
+# helper
+if (len(sys.argv) <= 1):
+    print bcolor.TEXT + '=== INSTRUCTION OF HOW TO USE SCRIPT ===' + bcolor.ENDC
+    print "1. Type in Terminal: python install.py [pathToExtension] [pathToMagentoWebroot]"
+    print "2. DONE"
+    sys.exit()
+
+
 #print 'Number of arguments: ', str(sys.argv)
 arg = sys.argv
 extension = arg[1]
 webroot = arg[2]
 
 # create connection 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="human01", db="test")
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="human01", db="totaltool")
 
 # prepare a cursor object using cursor() method
 cursor = conn.cursor()
@@ -45,8 +58,6 @@ if data:
     except MySQLdb.Warning, e:
         print str(e) 
 
-#sys.exit()
-
 # copy Magento 2 extension to destination directory: app / code
 print "copy new temando extension..."
 time.sleep(2)
@@ -58,22 +69,25 @@ os.system(cpCmd)
 print "run magento upgrade..."
 time.sleep(2)
 upgradeCmd = 'php ' + webroot + 'bin/magento setup:upgrade'
-#os.system(upgradeCmd)
-
+os.system(upgradeCmd)
 
 # clean all Magento cache: cache, generation and page_cache folder
 print "Magento cache cleaning..."
 time.sleep(2)
-cleanCmd = 'rm -rf '  + webroot + 'var/cache' + ' ' + webroot + 'var/generation' + ' ' + webroot + 'var/page_cache';
+cleanCmd = 'rm -rf '  + webroot + 'var/cache' + ' ' + webroot + 'var/generation' + ' ' + webroot + 'var/page_cache'
 os.system(cleanCmd)
-
 
 # generate magento static content
 print "magento static content generate..."
 time.sleep(2)
-#generateCmd = 'php ' + webroot + 'bin/magento setup:static-content:deploy'
-#os.system(generateCmd)
+generateCmd = 'php ' + webroot + 'bin/magento setup:static-content:deploy'
+os.system(generateCmd)
+
+# generate magento static content
+print "set permission for var and pub/static..."
+time.sleep(2)
+generateCmd = 'chmod -R 777 ' + webroot + 'var/cache' + ' ' + webroot + 'var/generation' + ' ' + webroot + 'var/page_cache' + ' ' + webroot + 'pub/static'
+os.system(generateCmd)
 
 # complete 
 print "COMPLETED..."
-time.sleep(2)
